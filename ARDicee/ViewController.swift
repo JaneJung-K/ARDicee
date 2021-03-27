bf4c0ff044b13477bb6999316ea06b078fc87283
JaneJung <piaojane@daum.net>
2021-03-27 20:28:42 +0900

    Find and Import 3D Models for AR

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -21,31 +21,39 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         //let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01) //사각형
         
-        let sphere = SCNSphere(radius: 0.2)
-        
-        let material = SCNMaterial() //물체 표면 속성
-        
-        material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
-        
-        sphere.materials = [material]
-        
-        let node = SCNNode() //콘텐츠를 연결할 수 있는 3D 좌표 공간에서 위치와 변환을 나타내는 장면 그래프의 구조 요소
-        
-        node.position = SCNVector3(0, 0.1, -0.5)
-        
-        node.geometry = sphere
-        
-        sceneView.scene.rootNode.addChildNode(node)
+//        let sphere = SCNSphere(radius: 0.2)
+//
+//        let material = SCNMaterial() //물체 표면 속성
+//
+//        material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
+//
+//        sphere.materials = [material]
+//
+//        let node = SCNNode() //콘텐츠를 연결할 수 있는 3D 좌표 공간에서 위치와 변환을 나타내는 장면 그래프의 구조 요소
+//
+//        node.position = SCNVector3(0, 0.1, -0.5)
+//
+//        node.geometry = sphere
+//
+//        sceneView.scene.rootNode.addChildNode(node)
         
         //주사위가 3D처럼 보이지 않는다. 빛을 추가하자.
         sceneView.autoenablesDefaultLighting = true
         
         
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
-//        // Set the scene to the view
-//        sceneView.scene = scene
+        // Create a new scene
+        let dicescene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        
+        //실제적으로 Dice에는 recursively: true 할 것이 없지만 좋은 습관이다.
+        if let diceNode = dicescene.rootNode.childNode(withName: "Dice", recursively: true) {
+        
+        diceNode.position = SCNVector3(0, 0, -0.1)
+        
+        sceneView.scene.rootNode.addChildNode(diceNode)
+        
+        }
+
+       
     }
     
     override func viewWillAppear(_ animated: Bool) {
